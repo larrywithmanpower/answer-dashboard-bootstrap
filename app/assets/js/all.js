a47a563d4f04e534734fc166d67f72ce914e9cb5
LokiH <larry0708@hotmail.com>
2021-08-09 19:03:23 +0800

    index.html and edit.html completed

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,8 +2,25 @@
 // layout sideMenu 
 let loc = (window.location.pathname).substring(1);
 
-$('.menu__item').find('.link').each(function () {
-  if($(this).attr('href') == loc) {
-    $(this).parent().addClass('active');
-  }
-});
+if (loc === 'index.html') {
+  $('.menu__item').find('.link').each(function () {
+    if($(this).attr('href') === loc) {
+      $(this).parent().addClass('active');
+    }
+  });
+} else if (loc === 'edit.html') {
+  $('.menu__item').find('.link').each(function () {
+    if($(this).attr('href') === 'index.html') {
+      $(this).parent().addClass('active');
+    }
+  });
+} else if (loc === 'admin.html') {
+  $('.menu__item').find('.link').each(function () {
+    if($(this).attr('href') === loc) {
+      $(this).parent().addClass('active');
+      // ! 下面為了在未製作完成前加上，之後要取消
+      $('.menu').addClass('vh-100');
+    }
+  });
+}
+
