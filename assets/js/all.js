24ad7d2e5a60aa3ea7224a16cb4babd409d97c0d
LokiH <larry0708@hotmail.com>
2021-08-10 14:49:22 +0800

    Update 2021-08-10T06:49:19.334Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,46 +1,42 @@
+"use strict";
 
 // layout sideMenu 
-let loc = (window.location.pathname).substring(1);
+var loc = window.location.pathname.substring(1);
 
 if (loc === 'index.html') {
   $('.menu__item').find('.link').each(function () {
-    if($(this).attr('href') === loc) {
+    if ($(this).attr('href') === loc) {
       $(this).parent().addClass('active');
     }
   });
 } else if (loc === 'edit.html') {
   $('.menu__item').find('.link').each(function () {
-    if($(this).attr('href') === 'index.html') {
+    if ($(this).attr('href') === 'index.html') {
       $(this).parent().addClass('active');
     }
   });
 } else if (loc === 'admin.html') {
   $('.menu__item').find('.link').each(function () {
-    if($(this).attr('href') === loc) {
+    if ($(this).attr('href') === loc) {
       $(this).parent().addClass('active');
     }
   });
-}
+} // modal
 
 
-// modal
-let name;
-let email;
-
-$('.js-modal').click(function() {
+var name;
+var email;
+$('.js-modal').click(function () {
   name = $(this).data('name');
   email = $(this).data('email');
   console.log(name, email);
   $('.js-accountName').attr('value', name);
   $('.js-accountEmail').attr('value', email);
-});
+}); // infor modal
 
-
-// infor modal
-let infoItem;
-let infoName;
-let infoEmail;
-
+var infoItem;
+var infoName;
+var infoEmail;
 $('.js-checkModal').click(function () {
   infoItem = $(this).data('item');
   infoName = $(this).data('name');
@@ -48,11 +44,10 @@ $('.js-checkModal').click(function () {
   $('.js-checkModalItem').text(infoItem);
   $('.js-checkModalName').text(infoName);
   $('.js-checkModalEmail').text(infoEmail);
-});
+}); // toggle rotate
 
-
-// toggle rotate
 $(".js-rotate").click(function () {
   console.log($(this).find('i'));
   $(this).toggleClass("down");
-})
+});
+//# sourceMappingURL=all.js.map
