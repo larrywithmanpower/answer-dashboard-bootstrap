bf976f4f1279a9375843fbce69d4a5cc3913477f
LokiH <larry0708@hotmail.com>
2021-08-10 19:09:55 +0800

    修正sidemenu無法加入active的錯誤

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,21 +1,16 @@
 $(document).ready(function () {
-
-  // layout sideMenu 
-  let loc = window.location.pathname.substring(1);
-  console.log(loc);
-
-  $('.menu__item').find('.link').each(function () {
-    console.log($(this).attr('href'));
-    if ($(this).attr('href') === loc) {
-      $(this).parent().toggleClass('active');
-    }
-  });
-
+  // side menu
+  $('.menu__item .link').each(function () {
+    // console.log($(this)[0]);
+    if($(this)[0].href==String(window.location)){ 
+      $(this).parent().removeClass("active");
+      $(this).parent().addClass("active"); 
+    } 
+  })
 
   // edit area
   $('.js-editBtn').click(function (e) {
     e.preventDefault();
-    console.log('got you');
     $('.js-msgArea').addClass('d-none');
     $('.js-footerArea').addClass('d-none');
     $('.js-editArea').removeClass('d-none').addClass('d-block');
@@ -28,7 +23,6 @@ $(document).ready(function () {
   $('.js-modal').click(function () {
     name = $(this).data('name');
     email = $(this).data('email');
-    console.log(name, email);
     $('.js-accountName').attr('value', name);
     $('.js-accountEmail').attr('value', email);
   });
@@ -53,9 +47,20 @@ $(document).ready(function () {
   $(".js-rotate").click(function () {
     console.log($(this).find('i'));
     $(this).toggleClass("down");
-  })
+  });
 
-})
+});
 
+
+ // layout sideMenu 
+  // let loc = window.location.pathname.substring(1);
+  // console.log(loc);
+
+  // $('.menu__item').find('.link').each(function () {
+  //   console.log($(this).attr('href'));
+  //   if ($(this).attr('href') === loc) {
+  //     $(this).parent().toggleClass('active');
+  //   }
+  // });
 
 
