c1ef758699e8858d22aa6cb108a3484d36253333
LokiH <larry0708@hotmail.com>
2021-08-10 17:57:50 +0800

    Update 2021-08-10T09:57:47.430Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,45 +1,50 @@
 "use strict";
 
-// layout sideMenu 
-var loc = window.location.href;
-$('.menu__item').find('.link').each(function () {
-  if ($(this).attr('href') === loc) {
-    $(this).parent().toggleClass('active');
-  }
-}); // edit area
+$(document).ready(function () {
+  // layout sideMenu 
+  var loc = window.location.pathname.substring(1);
+  console.log(loc);
+  $('.menu__item').find('.link').each(function () {
+    console.log($(this).attr('href'));
 
-$('.js-editBtn').click(function (e) {
-  e.preventDefault();
-  console.log('got you');
-  $('.js-msgArea').addClass('d-none');
-  $('.js-footerArea').addClass('d-none');
-  $('.js-editArea').removeClass('d-none').addClass('d-block');
-}); // modal
+    if ($(this).attr('href') === loc) {
+      $(this).parent().toggleClass('active');
+    }
+  }); // edit area
 
-var name;
-var email;
-$('.js-modal').click(function () {
-  name = $(this).data('name');
-  email = $(this).data('email');
-  console.log(name, email);
-  $('.js-accountName').attr('value', name);
-  $('.js-accountEmail').attr('value', email);
-}); // infor modal
+  $('.js-editBtn').click(function (e) {
+    e.preventDefault();
+    console.log('got you');
+    $('.js-msgArea').addClass('d-none');
+    $('.js-footerArea').addClass('d-none');
+    $('.js-editArea').removeClass('d-none').addClass('d-block');
+  }); // modal
 
-var infoItem;
-var infoName;
-var infoEmail;
-$('.js-checkModal').click(function () {
-  infoItem = $(this).data('item');
-  infoName = $(this).data('name');
-  infoEmail = $(this).data('email');
-  $('.js-checkModalItem').text(infoItem);
-  $('.js-checkModalName').text(infoName);
-  $('.js-checkModalEmail').text(infoEmail);
-}); // toggle rotate
+  var name;
+  var email;
+  $('.js-modal').click(function () {
+    name = $(this).data('name');
+    email = $(this).data('email');
+    console.log(name, email);
+    $('.js-accountName').attr('value', name);
+    $('.js-accountEmail').attr('value', email);
+  }); // infor modal
 
-$(".js-rotate").click(function () {
-  console.log($(this).find('i'));
-  $(this).toggleClass("down");
+  var infoItem;
+  var infoName;
+  var infoEmail;
+  $('.js-checkModal').click(function () {
+    infoItem = $(this).data('item');
+    infoName = $(this).data('name');
+    infoEmail = $(this).data('email');
+    $('.js-checkModalItem').text(infoItem);
+    $('.js-checkModalName').text(infoName);
+    $('.js-checkModalEmail').text(infoEmail);
+  }); // toggle rotate
+
+  $(".js-rotate").click(function () {
+    console.log($(this).find('i'));
+    $(this).toggleClass("down");
+  });
 });
 //# sourceMappingURL=all.js.map
