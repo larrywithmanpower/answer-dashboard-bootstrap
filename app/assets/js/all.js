e29388ff899b66fa81c6a0793adb30654f8febf7
LokiH <larry0708@hotmail.com>
2021-08-10 17:56:37 +0800

    修正all.js，在路由判斷上仍然失敗，本地端沒問題，但上傳gh-page後就無法取得正確的href

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,52 +1,61 @@
+$(document).ready(function () {
 
-// layout sideMenu 
-let loc = window.location.href;
+  // layout sideMenu 
+  let loc = window.location.pathname.substring(1);
+  console.log(loc);
 
-$('.menu__item').find('.link').each(function () {
-  if($(this).attr('href') === loc) {
-    $(this).parent().toggleClass('active');
-  }
-});
+  $('.menu__item').find('.link').each(function () {
+    console.log($(this).attr('href'));
+    if ($(this).attr('href') === loc) {
+      $(this).parent().toggleClass('active');
+    }
+  });
 
-// edit area
-$('.js-editBtn').click(function (e) {
-  e.preventDefault();
-  console.log('got you');
-  $('.js-msgArea').addClass('d-none');
-  $('.js-footerArea').addClass('d-none');
-  $('.js-editArea').removeClass('d-none').addClass('d-block');
+
+  // edit area
+  $('.js-editBtn').click(function (e) {
+    e.preventDefault();
+    console.log('got you');
+    $('.js-msgArea').addClass('d-none');
+    $('.js-footerArea').addClass('d-none');
+    $('.js-editArea').removeClass('d-none').addClass('d-block');
+  })
+
+  // modal
+  let name;
+  let email;
+
+  $('.js-modal').click(function () {
+    name = $(this).data('name');
+    email = $(this).data('email');
+    console.log(name, email);
+    $('.js-accountName').attr('value', name);
+    $('.js-accountEmail').attr('value', email);
+  });
+
+
+  // infor modal
+  let infoItem;
+  let infoName;
+  let infoEmail;
+
+  $('.js-checkModal').click(function () {
+    infoItem = $(this).data('item');
+    infoName = $(this).data('name');
+    infoEmail = $(this).data('email');
+    $('.js-checkModalItem').text(infoItem);
+    $('.js-checkModalName').text(infoName);
+    $('.js-checkModalEmail').text(infoEmail);
+  });
+
+
+  // toggle rotate
+  $(".js-rotate").click(function () {
+    console.log($(this).find('i'));
+    $(this).toggleClass("down");
+  })
+
 })
 
-// modal
-let name;
-let email;
-
-$('.js-modal').click(function() {
-  name = $(this).data('name');
-  email = $(this).data('email');
-  console.log(name, email);
-  $('.js-accountName').attr('value', name);
-  $('.js-accountEmail').attr('value', email);
-});
 
 
-// infor modal
-let infoItem;
-let infoName;
-let infoEmail;
-
-$('.js-checkModal').click(function () {
-  infoItem = $(this).data('item');
-  infoName = $(this).data('name');
-  infoEmail = $(this).data('email');
-  $('.js-checkModalItem').text(infoItem);
-  $('.js-checkModalName').text(infoName);
-  $('.js-checkModalEmail').text(infoEmail);
-});
-
-
-// toggle rotate
-$(".js-rotate").click(function () {
-  console.log($(this).find('i'));
-  $(this).toggleClass("down");
-})
