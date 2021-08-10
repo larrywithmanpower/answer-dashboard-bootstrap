3b7c92ba4d74542085320100dfa19b236607d361
LokiH <larry0708@hotmail.com>
2021-08-10 17:10:35 +0800

    all.js modify addClass to toggleClass at .emnu__item.

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -4,7 +4,7 @@ let loc = window.location.pathname.substring(1);
 
 $('.menu__item').find('.link').each(function () {
   if($(this).attr('href') === loc) {
-    $(this).parent().addClass('active');
+    $(this).parent().toggleClass('active');
   }
 });
 
